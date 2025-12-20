0e702a0b5253312ecfd6ba9ef57993daa3545456
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2025-12-20 10:08:25 +0530

    Add files via upload

---
+++ fourth.1.js
@@ -0,0 +1,11 @@
+let array=["microsoft","uber","google","IBM","netflix"];
+console.log(array);
+//1
+let remove=array.shift("microsoft");
+console.log(remove);
+//2
+let remove1=array.splice(1,1,"ola");
+console.log(remove1);
+//3
+let add=array.push("amazon");
+console.log(add);
