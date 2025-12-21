c4d10e0937d804c8e2aeb99b7d5fc9dfa6598ff8
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2025-12-21 09:57:33 +0530

    Add files via upload

---
+++ secound.js
@@ -0,0 +1,72 @@
+// this is my comment single line comment
+/* this code prints hello world -multi line comment */
+console.log("hello world");
+// arithmatc operations
+/*
+let a = 5;
+let b= 2;
+console.log("sum", a + b);
+console.log("subtraction", a - b);
+console.log("multiplication", a * b);
+console.log("division", a / b);
+console.log("modulus", a % b);
+*/
+//unary operators
+/*
+let a = 5;
+let b = 2;
+a=a+1;
+console.log("increment",a);
+b=b-1;
+console.log("decrement",b);
+*/
+/*
+// assignment operators
+let a = 5;
+a += 2; // a = a + 2
+console.log("assign", a);
+*/
+/*
+//comparison operators
+let a = 5;
+let b = 2;
+console.log("equal to", a == b);
+console.log("a>b", a > b);
+console.log("a<b", a < b);
+console.log("a>=b", a >= b);
+console.log("a<=b", a <= b);
+console.log("a!=b", a != b);
+*/
+/*
+// logical operators
+let a = true;
+let b = false;
+console.log("a and b", a && b);
+console.log("a or b", a || b);
+console.log("not a", !a);
+*/
+/*
+//conditional operator
+let age=25
+if(age >= 18)
+{
+    console.log("you are eligible to vote");
+}
+*/
+/*
+//if-else statement
+let age=25
+if(age >= 18)
+{
+    console.log("you are eligible to vote");
+}
+else
+{
+    console.log("you are not eligible to vote");
+}
+*/
+/*
+let age=25;
+age>=18 ? console.log("eligible to vote") : console.log("not eligible to vote");
+*/
+
