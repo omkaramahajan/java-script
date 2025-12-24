a5d6d2f8eeea2fad03f26fe168b3d639c07c86b9
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2025-12-24 18:34:02 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,16 @@
+/*
+//LOOP
+for (let i = 1; i <= 5; i++)
+{
+    console.log("OMKAR");
+}
+*/
+// string
+str = "OMKAR";
+console.log(str.length);
+console.log(str);
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[2]);
+console.log(str[3]);
+console.log(str[4]);
