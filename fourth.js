0e058f38eeae35646a7689196e80ce1eb69bcbe0
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2025-12-29 16:04:37 +0530

    Add files via upload

---
+++ fourth.js
@@ -0,0 +1,12 @@
+// array
+let marks=[34,45,67,89,23,90];
+console.log(marks);
+console.log(marks.length);
+marks[2]=100;
+console.log(marks);
+
+cityes=["karachi","lahore","islamabad","quetta"];
+console.log(cityes);
+console.log(cityes.length);
+cityes[1]="peshawar";
+console.log(cityes);
