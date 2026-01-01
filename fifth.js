e27ba538251c4d35c6def6584db928b66ee2e66f
𝙊𝙈𝙆𝘼𝙍 𝙅𝙄𝙏𝙀𝙉𝘿𝙍𝘼 𝙈𝘼𝙃𝘼𝙅𝘼𝙉𝘼 <omkarmahajan250506@gmail.com>
2026-01-01 19:23:54 +0530

    Add files via upload

---
+++ fifth.js
@@ -0,0 +1,17 @@
+/*
+// function
+function abc()
+{
+    console.log("hello");
+}
+function my func(abc){
+    return abc;
+}
+*/
+let arr=[1,2,3,4,5];
+
+arr.forEach((val)=>
+{
+    console.log(val);
+
+});
